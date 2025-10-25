21522b94de2e22b8158081b5dc6c6e4cf434fa56
olefSch <olinho22@gmail.com>
2025-10-25 17:30:18 +0200

    cd(vite-config): better vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  //base: "/my_portfolio/", 
+  base: mode === "production" ? "/my_portfolio/" : "/",
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
