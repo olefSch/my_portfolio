688328e72e3170f6512f4fd3e0ab2b9c87905a6d
olefSch <olinho22@gmail.com>
2025-10-25 17:41:35 +0200

    cd(deployment): own domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/my_portfolio/" : "/",
+  base: mode === "production" ? "/" : "/",
 
   plugins: [
     react(),
